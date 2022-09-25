072559248e1ed1f0744cddb53a2fae16512b38df
benecia06 <goel.sakshi25@icloud.com>
2022-09-25 19:48:39 +0530

    Type casting1

--- datatypes.py
+++ datatypes.py
@@ -15,3 +15,6 @@ num_char= len(input("What is your name? "))
 # print("The length is" + num_char+ "characters") ERROR: TypeError: can only concatenate str (not "int") to str
 new_num_char= str(num_char)
 print("The length is " + new_num_char+ " characters") #Yay no error!
+print(type(b)) #type() is used to get the datatype of a variable
+print(123+ float("100.2"))
+print(str(70)+ str(100))
