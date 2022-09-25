de25b8fcde944bc5f5081a62c25877921e641ce4
benecia06 <goel.sakshi25@icloud.com>
2022-09-25 21:58:25 +0530

    Q. Life in weeks

---
+++ lifeinweeks.py
@@ -0,0 +1,8 @@
+age = input("What is your current age?")
+days= (90-int(age))*365
+weeks= (90-int(age))*52
+months= (90-int(age))*12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
--- operators.py
+++ operators.py
@@ -9,7 +9,6 @@ mul= 3*2
 div_quotient= 4/3
 div_remainder= 4%3
 power= 2**3
-square_root= 4//2
 # Precedence of Operators
 # PEMDASLR(LEFT TO RIGHT)
 # PARENTHESES->()
@@ -17,6 +16,18 @@ square_root= 4//2
 # MULTIPLICATION-> * DIVISION-> / 
 # ADDITION-> + SUBTACTION-> - (IN CASE OF EQUAL PRECEDENCE THE OPERATOR ON THE LEFT IS GIVEN PRIORITY)
 print(3 * (3 + 3) / 3 - 3)
+
+print(round(8/3, 2)) #round() is used to round off a number to specific decimal places by adding the required place after the comma
+print(8//3) #// is used to chop off the decimal points in a number
+print(8/3)
+add+=1 #shorthand notation 
+#fstring
+score= 10
+height= 1.8
+isWinning= True
+print(f"Your score is {score}, your height is {height} and you are winning is {isWinning}") #f-string is used to avoid unnecessary type conversions
+
+
 
 
 
