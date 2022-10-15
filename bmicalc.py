76822ec20da56acc55e198d27067065dc7642bd3
benecia06 <goel.sakshi25@icloud.com>
2022-10-15 18:49:14 +0530

    BMI CALCULATOR 2.0

--- bmicalc.py
+++ bmicalc.py
@@ -6,4 +6,10 @@ weight = input("enter your weight in kg: ")
 new_wt= float(weight)
 new_ht= float(height)
 BMI= (new_wt/(new_ht**2))
-print(int(BMI))
+
+if BMI<18.5: print(f"Your BMI is {round(BMI)}, you are underweight.")
+elif BMI>18.5 and BMI<25: print(f"Your BMI is {round(BMI)}, you have a normal weight.")
+elif BMI>25 and BMI<30: print(f"Your BMI is {round(BMI)}, you are slightly overweight.")
+elif BMI>30 and BMI<35: print(f"Your BMI is {round(BMI)}, you are obese.")
+elif BMI>35: print(f"Your BMI is {round(BMI)}, you are clinically obese.")
+
--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,14 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-if height>120:
+if height>=120: 
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age<12:
+        print("Ticket price= $5")
+    elif age<=18:
+        print("Ticket price= $10")
+    else:
+        print("Ticket price= $12")
+
 else:
      print("Sorry, you cannot ride the rollercoaster.")
